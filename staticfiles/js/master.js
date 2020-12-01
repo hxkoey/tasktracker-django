d617c4f4c2a7c6511005a19350337e7621ef5727
hxkoey <koey.huixin@gmail.com>
2020-12-02 00:19:13 +0800

    first-commit

---
+++ staticfiles/js/master.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  
+  $(".datetimeinput").datepicker({changeYear: true,changeMonth: true, dateFormat: 'yy-mm-dd'});
+  $(".dateinput").datepicker({changeYear: true,changeMonth: true, dateFormat: 'yy-mm-dd'});
+  $("#datepicker").datepicker();
+ 
+ 
+});

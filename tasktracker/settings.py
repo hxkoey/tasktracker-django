65b026adeaed7f33e6a7b1e114bda4407e553ab6
hxkoey <koey.huixin@gmail.com>
2020-12-02 00:59:23 +0800

    second-commit

--- tasktracker/settings.py
+++ tasktracker/settings.py
@@ -27,7 +27,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['tasktracker-django.herokuapp.com','127.0.0.1']
 

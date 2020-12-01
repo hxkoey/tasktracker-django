6a25b318b3ac82eae48667b70adbfc6c281ab561
hxkoey <koey.huixin@gmail.com>
2020-12-02 00:28:33 +0800

    second commit

--- tasktrackerApp/views.py
+++ tasktrackerApp/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from django.urls import reverse
 from django.core.paginator import Paginator
 
 # Create your views here.
